c92e0a80fe5e751f6a4cfcda4bcfde08b7a51386
md ask <mohammedmaheswer12@gmail.com>
2024-03-16 16:34:11 +0530

    delete log

--- main/deletedriver.php
+++ main/deletedriver.php
@@ -4,4 +4,5 @@ include "main/session.php";
 ob_start();
 $id = $_GET['hakuna'];
 $res = $obj->delete("users", $id);
+$obj->saveactivity("Driver Deleted by Admin", "", $id, $employeeid, "Driver", "Driver Deleted by Admin");
 echo "Driver Deleted Successfully";
--- main/deletevehicle.php
+++ main/deletevehicle.php
@@ -4,4 +4,5 @@ include "main/session.php";
 ob_start();
 $id = $_GET['hakuna'];
 $res = $obj->delete("vehicles", $id);
+$obj->saveactivity("Vehicle Deleted by Admin", "", $id, $employeeid, "Vehicle", "Vehicle Deleted by Admin");
 echo "Driver Deleted Successfully";
