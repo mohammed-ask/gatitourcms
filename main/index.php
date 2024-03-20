6b49f5969e8ccb04c5e800c142214bc657e2151a
husain Bandookwala <mohammedmaheswer12@gmail.com>
2024-03-20 14:38:57 +0530

    index

--- main/index.php
+++ main/index.php
@@ -1,27 +1,283 @@
-<?php
-include "main/session.php";
-/* @var $obj db */
-ob_start();
-$totalcutomer = $obj->selectfieldwhere("users", "count(id)", "status=1 and type=2");
-$totaldriver = $obj->selectfieldwhere("users", "count(id)", "status=1 and type=3");
-$totalvehicle = $obj->selectfieldwhere("vehicles", "count(id)", "status=1 ");
-?>
-<style>
-    #datacards a {
-        color: white;
-    }
-</style>
-<div class="container px-6 mx-auto grid mobile-bottom-margin">
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
 
-</div>
-<?php
-// }
-//Assign all Page Specific variables
-$pagemaincontent = ob_get_contents();
-ob_end_clean();
-$pagemeta = "";
-$pagetitle = "Gati Tour: Admin Dashboard";
-$contentheader = "";
-$pageheader = "";
-include "main/templete.php";
-?>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>GatiTour | Travel Agency Landing Page UI</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="main/dist/assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="main/dist/assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="main/dist/assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="main/dist/assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="main/dist/assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="main/dist/assets/img/favicons/mstile-150x150.png"> -->
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="main/dist/assets/css/theme.css" rel="stylesheet" />
+
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top pl-2 pt-4 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
+        <div class="container"><a class="navbar-brand" href="index.html"><img src="main/dist/assets/img/Gatitour(Dark).png" height="50" alt="logo" /></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"> </span></button>
+          <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base align-items-lg-center align-items-start">
+             
+              <li class="nav-item px-3 px-xl-4"><a class="nav-link fw-medium" aria-current="page" href="#downloadapp">Download App</a></li>
+
+              <li class="nav-item px-3 px-xl-4"><a class="btn btn-outline-dark order-1 order-lg-0 fw-medium" href="#!">Admin Login</a></li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section style="padding-top: 7rem;">
+        <div class="bg-holder" style="background-image:url(main/dist/assets/img/hero/hero-bg.svg);">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-5 col-lg-6 order-0 order-md-1 text-end"><img class="pt-7 pt-md-0 hero-img" src="main/dist/assets/img/hero/hero-img.png" alt="hero-header" /></div>
+            <div class="col-md-7 col-lg-6 text-md-start text-center py-6">
+              <h4 class="fw-bold text-danger mb-3">Best Destinations around the India</h4>
+              <h1 class="hero-title">Travel, enjoy and live a new and full life</h1>
+              <p class="mb-4 fw-medium">At Gatitour, we're dedicated to transforming your travel experiences into unforgettable adventures.<br class="d-none d-xl-block" /> Explore the world with confidence and convenience with Gatitour by your side.</p>
+              <div class="text-center text-md-start"> <a class="btn btn-primary btn-lg me-md-4 mb-3 mb-md-0 border-0 primary-btn-shadow" href="#downloadapp" role="button">Just Download App</a>
+                <!-- <div class="w-100 d-block d-md-none"></div><a href="#!" role="button" data-bs-toggle="modal" data-bs-target="#popupVideo"><span class="btn btn-danger round-btn-lg rounded-circle me-3 danger-btn-shadow"> <img src="main/dist/assets/img/hero/play.svg" width="15" alt="paly"/></span></a><span class="fw-medium">Play Demo</span>
+                <div class="modal fade" id="popupVideo" tabindex="-1" aria-labelledby="popupVideo" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content">
+                      <iframe class="rounded" style="width:100%;max-height:500px;" height="500px" src="https://www.youtube.com/embed/_lhdhL4UDIo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+                    </div>
+                  </div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="pt-5 pt-md-9" id="service">
+
+        <div class="container">
+          <div class="position-absolute z-index--1 end-0 d-none d-lg-block"><img src="main/dist/assets/img/category/shape.svg" style="max-width: 200px" alt="service" /></div>
+          <div class="mb-7 text-center">
+            <h5 class="text-secondary">CATEGORY </h5>
+            <h3 class="fs-xl-10 fs-lg-8 fs-7 fw-bold font-cursive text-capitalize">Why Choose Gatitour?     </h3>
+          </div>
+          <div class="row">
+            <div class="col-lg-6 col-sm-12 mb-6">
+              <div class="card service-card shadow-hover rounded-3 text-center align-items-center">
+                <div class="card-body p-xxl-5 p-4"> <img src="main/dist/assets/img/category/icon1.png" width="150" alt="Service" />
+                  <h4 class="mb-3">Extensive Vehicle Fleet</h4>
+                  <p class="mb-0 fw-medium">Choose from our selection of: Hatchback Cars,
+                    Sedan Cars,
+                    SUV Cars,
+                    Tata Wingers,
+                    Maxx Toofans
+                    Tempo Travellers,
+                    Buses,</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-sm-12 mb-6">
+              <div class="card service-card shadow-hover rounded-3 text-center align-items-center">
+                <div class="card-body p-xxl-5 p-4"> <img src="main/dist/assets/img/category/icon2.png" width="75" alt="Service" />
+                  <h4 class="mb-3">Convenient Booking</h4>
+                  <p class="mb-0 fw-medium">Download our user-friendly app to:
+                    Vehicle options,  
+                    Make bookings on the go,
+                    instant confirmation,
+                    24/7 customer support</p>
+                </div>
+              </div>
+            </div>
+                   </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="pt-5" id="destination">
+
+        <div class="container">
+          <div class="position-absolute start-100 bottom-0 translate-middle-x d-none d-xl-block ms-xl-n4"><img src="main/dist/assets/img/dest/shape.svg" alt="destination" /></div>
+          <div class="mb-7 text-center">
+            
+            <h3 class="fs-xl-10 fs-lg-8 fs-7 fw-bold font-cursive text-capitalize">Top Destinations</h3>
+          </div>
+          <div class="row">
+            <div class="col-md-4 mb-4">
+              <div class="card overflow-hidden shadow"> <img class="card-img-top" src="main/dist/assets/img/dest/dest1.jpg" alt="Rome, Italty" />
+             
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <div class="card overflow-hidden shadow"> <img class="card-img-top" src="main/dist/assets/img/dest/dest2.jpg" alt="London, UK" />
+              </div>
+            </div>
+            <div class="col-md-4 mb-4">
+              <div class="card overflow-hidden shadow"> <img class="card-img-top" src="main/dist/assets/img/dest/dest3.jpg" alt="Full Europe" />
+  
+              </div>
+            </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="booking">
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="mb-4 text-start">
+                <h5 class="text-secondary">Easy and Fast </h5>
+                <h3 class="fs-xl-10 fs-lg-8 fs-7 fw-bold font-cursive text-capitalize">Book your next trip in 3 easy steps</h3>
+              </div>
+              <div class="d-flex align-items-start mb-5">
+                <div class="bg-primary me-sm-4 me-3 p-3" style="border-radius: 13px"> <img src="main/dist/assets/img/steps/selection.svg" width="22" alt="steps" /></div>
+                <div class="flex-1">
+                  <h5 class="text-secondary fw-bold fs-0">Download GatiTour App</h5>
+                  <p>Download our GatiTour App, <br class="d-none d-sm-block"> and explore the App</p>
+                </div>
+              </div>
+              <div class="d-flex align-items-start mb-5">
+                <div class="bg-danger me-sm-4 me-3 p-3" style="border-radius: 13px"> <img src="main/dist/assets/img/steps/water-sport.svg" width="22" alt="steps" /></div>
+                <div class="flex-1">
+                  <h5 class="text-secondary fw-bold fs-0">Choose Destination</h5>
+                  <p>Choose your favourite place. No matter <br class="d-none d-sm-block"> where you travel inside in India.</p>
+                </div>
+              </div>
+              <div class="d-flex align-items-start mb-5">
+                <div class="bg-info me-sm-4 me-3 p-3" style="border-radius: 13px"> <img src="main/dist/assets/img/steps/taxi.svg" width="22" alt="steps" /></div>
+                <div class="flex-1">
+                  <h5 class="text-secondary fw-bold fs-0">Get Confirm Booking</h5>
+                  <p>Lastly, you will get confirm booking <br class="d-none d-sm-block">  and enjoy the vacation.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center align-items-start">
+              <div class="card position-relative shadow" style="max-width: 370px;">
+                <div class="position-absolute z-index--1 me-10 me-xxl-0" style="right:-160px;top:-210px;"> <img src="main/dist/assets/img/steps/bg.png" style="max-width:550px;" alt="shape" /></div>
+                <div class="card-body p-3"> <img class="mb-0 mt-0 rounded-2 w-100" src="main/dist/assets/img/steps/booking-img.jpg" alt="booking" />
+                  <div>
+                    
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="downloadapp" class="pt-6">
+
+        <div class="container">
+          <div class="py-8 px-5 position-relative text-center" style="background-color: rgba(223, 215, 249, 0.199);border-radius: 129px 20px 20px 20px;">
+            <div class="position-absolute start-100 top-0 translate-middle ms-md-n3 ms-n4 mt-3"> <img src="main/dist/assets/img/cta/send.png" style="max-width:70px;" alt="send icon" /></div>
+            <div class="position-absolute end-0 top-0 z-index--1"> <img src="main/dist/assets/img/cta/shape-bg2.png" width="264" alt="cta shape" /></div>
+            <div class="position-absolute start-0 bottom-0 ms-3 z-index--1 d-none d-sm-block"> <img src="main/dist/assets/img/cta/shape-bg1.png" style="max-width: 340px;" alt="cta shape" /></div>
+            <div class="row justify-content-center">
+              <div class="col-lg-8 col-md-10">
+                <h2 class="text-secondary lh-1-7 mb-7">Download Gati Tour App</h2>
+                
+                  <div class="col-sm-auto">
+                    <button class="btn btn-danger orange-gradient-btn fs--1"><a href="main/dist/assets/video/beach.mp4" download="GatiTour App">Click to Download App</a></button>
+                  </div>
+            
+              </div>
+            </div>
+          </div>
+        </div><!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+
+
+
+      <!-- <div class="col-lg-3 col-md-5 col-12 mb-4 mb-md-6 mb-lg-0 order-lg-4 order-md-1">
+        <div class="icon-group mb-4"> <a class="text-decoration-none icon-item shadow-social" id="facebook" href="#!"><i class="fab fa-facebook-f"> </i></a><a class="text-decoration-none icon-item shadow-social" id="instagram" href="#!"><i class="fab fa-instagram"> </i></a><a class="text-decoration-none icon-item shadow-social" id="twitter" href="#!"><i class="fab fa-twitter"> </i></a></div>
+        <h4 class="fw-medium font-sans-serif text-secondary mb-3">Discover our app</h4>
+        <div class="d-flex align-items-center"> <a href="#!"> <img class="me-2" src="main/dist/assets/img/play-store.png" alt="play store" /></a><a href="#!"> <img src="main/dist/assets/img/apple-store.png" alt="apple store" /></a></div>
+      </div> -->
+
+
+      <div class="py-5 text-center">
+        <p class="mb-0 text-secondary fs--1 fw-medium">All rights reserved@gatitour.com </p>
+      </div>
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="main/dist/vendors/@popperjs/popper.min.js"></script>
+    <script src="main/dist/vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="main/dist/vendors/is/is.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="main/dist/vendors/fontawesome/all.min.js"></script>
+    <script src="main/dist/assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&amp;family=Volkhov:wght@700&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
