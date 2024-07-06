de8de351ea43f853544d941fa19b07223e6a3c48
md ask <mohammedmaheswer12@gmail.com>
2024-07-06 11:53:30 +0530

    pssword

--- main/helpline.php
+++ main/helpline.php
@@ -6,6 +6,10 @@ include "session.php";
         <span class="text-gray-700 dark:text-gray-400">Helpline No.</span>
         <input name="phone" value="<?= $helpline ?>" data-bvalidator="required,maxlength[10],minlength[10]" class="form-control" placeholder="Helpline No" />
     </label>
+    <label class="block text-sm" style="margin-bottom: 5px;">
+        <span class="text-gray-700 dark:text-gray-400">Change Password</span>
+        <input name="password" value="" data-bvalidator="maxlength[6],minlength[6]" class="form-control" placeholder="Password" />
+    </label>
     <div>
         <button type="submit" id="modalsubmit" class="w-full px-3 py-1 mt-6 text-sm font-medium hidden">
             Submit
--- main/inserthelpline.php
+++ main/inserthelpline.php
@@ -1,5 +1,9 @@
 <?php
 include "session.php";
+if(!empty($_POST['password'])){
+$sy["password"] = $_POST['password'];
+$pass = $obj->updatewhere("users", $sy, "id=".$employeeid."");
+}
 $xx["phone"] = $_POST['phone'];
 $help = $obj->updatewhere("personal_detail", $xx, "status=11");
 if ($help > 0) {
