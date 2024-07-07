a612e82f1a05401d03e51abc6c0c17437f24278b
md ask <mohammedmaheswer12@gmail.com>
2024-07-07 19:05:39 +0530

    Update helpline.php

--- main/helpline.php
+++ main/helpline.php
@@ -4,11 +4,11 @@ include "session.php";
 <form style="overflow-x: hidden;" id="adduser" onsubmit="event.preventDefault();sendForm('', '', 'inserthelpline', 'resultid', 'adduser');return 0;">
     <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Helpline No.</span>
-        <input name="phone" value="<?= $helpline ?>" data-bvalidator="required,minlength[10]" class="form-control" placeholder="Helpline No" />
+        <input name="phone" value="<?= $helpline ?>" data-bvalidator="required,maxlength[10],minlength[10]" class="form-control" placeholder="Helpline No" />
     </label>
     <label class="block text-sm" style="margin-bottom: 5px;">
         <span class="text-gray-700 dark:text-gray-400">Change Password</span>
-        <input name="password" value="" data-bvalidator="maxlength[6],minlength[6]" class="form-control" placeholder="Password" />
+        <input name="password" value="" data-bvalidator="minlength[6]" class="form-control" placeholder="Password" />
     </label>
     <div>
         <button type="submit" id="modalsubmit" class="w-full px-3 py-1 mt-6 text-sm font-medium hidden">
